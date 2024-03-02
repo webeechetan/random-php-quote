6f2f03a8c803d9278f1becb9b00d74ecc09d4000
Chetan Singh <chetan.singh@webeesocial.com>
2024-03-02 11:21:23 +0530

    new commit

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use Chetan\RandomPhpQuote\RandomPhpQuote;
+use Chetansingh\Quote\Quote;
 
-$quote = new RandomPhpQuote();
+$quote = new Quote();
 echo $quote->getQuote();
-
 ?>
---
+++ src/Quote.php
@@ -0,0 +1,11 @@
+<?php
+namespace Chetansingh\Quote;
+
+class Quote
+{
+    public function getQuote()
+    {
+        return "This is a quote";
+    }
+}
+?>
