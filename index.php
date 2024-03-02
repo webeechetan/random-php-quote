e460933d9ad9e8a92c91215195cb55ad4472ac31
Chetan Singh <chetan.singh@webeesocial.com>
2024-03-02 12:25:01 +0530

    added feature to ger  random quote

--- index.php
+++ index.php
@@ -4,5 +4,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Chetansingh\Quote\Quote;
 
 $quote = new Quote();
-echo $quote->getQuote();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <?php echo $quote->displayAsBootstrapCard();  ?>
+</body>
+</html>
--- src/Quote.php
+++ src/Quote.php
@@ -1,11 +1,94 @@
 <?php
 namespace Chetansingh\Quote;
+use GuzzleHttp\Client;
+
 
 class Quote
 {
+
+    public $quote;
+    public $author;
+    public $tags;
+
+    public function __construct()
+    {
+        $this->getQuote();
+    }
+
     public function getQuote()
     {
-        return "This is a quote";
+        $client = new Client();
+        $response = $client->request('GET','https://api.quotable.io/random');
+        $body = $response->getBody();
+        $data = json_decode($body);
+        $this->quote = $data->content;
+        $this->author = $data->author;
+        $this->tags = $data->tags;
+        $this->tags = implode(", ", $this->tags);
+        return $this->quote;
     }
+
+    public function displayAsBootstrapCard()
+    {
+        return "<div class='card'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>{$this->quote}</h5>
+                        <h6 class='card-subtitle mb-2 text-muted'>{$this->author}</h6>
+                        <p class='card-text'>{$this->tags}</p>
+                    </div>
+                 </div>";
+    }
+
+    public function displayAsText()
+    {
+        return "{$this->quote} - {$this->author}";
+    }
+
+    public function displayAsJson()
+    {
+        return json_encode([
+            'quote' => $this->quote,
+            'author' => $this->author,
+            'tags' => $this->tags
+        ]);
+    }
+
+    public function displayAsArray()
+    {
+        return [
+            'quote' => $this->quote,
+            'author' => $this->author,
+            'tags' => $this->tags
+        ];
+    }
+
+    public function displayAsHtml()
+    {
+        return "<p>{$this->quote} - {$this->author}</p>";
+    }
+
+    public function displayAsHtmlList()
+    {
+        return "<ul>
+                    <li>{$this->quote}</li>
+                    <li>{$this->author}</li>
+                    <li>{$this->tags}</li>
+                </ul>";
+    }
+
+    public function displayAsTextList()
+    {
+        return "{$this->quote}\n{$this->author}\n{$this->tags}";
+    }
+
+    public function displayAsJsonList()
+    {
+        return json_encode([
+            'quote' => $this->quote,
+            'author' => $this->author,
+            'tags' => $this->tags
+        ]);
+    }
+
 }
 ?>
